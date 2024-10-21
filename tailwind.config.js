9c79f0476687bb4c4e37af1ac1da179fc1d7adce
dwWebApp <123057170+dwWebApp@users.noreply.github.com>
2024-10-21 12:55:30 +0900

    first

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'LIGHT': ['CJ ONLYONE NEW BODY LIGHT'],
+      'REGULAR': ['CJ ONLYONE NEW BODY REGULAR'],
+      'MEDIUM': ['CJ ONLYONE NEW TITLE MEDIUM'],
+      'BOLD': ['CJ ONLYONE NEW TITLE BOLD']
+    },
+    backgroundPosition: {
+      'center-left': 'center left 17px',
+      'center-right': 'center right 10px',
+      'right': 'right',
+      'center-bottom': 'center bottom -25%',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
